cc06148b13b70e2cdd00acfcc07fb6b56635a0ae
dev15031997 <devashishpal15@gmail.com>
2025-03-03 23:58:31 +0530

    authentication and user route updated

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config({});
 
@@ -20,6 +21,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user", userRoute);
+
+
 
 app.listen(PORT,()=>{
     connectDB();
