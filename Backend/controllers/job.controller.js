d8289adbd1be5cc21dc5051a86c62bfd087ea159
dev15031997 <devashishpal15@gmail.com>
2025-03-05 01:22:22 +0530

    Application controller and routes created

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -66,9 +66,10 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId).populate({
-            path:"applications"
-        });
+        // const job = await Job.findById(jobId).populate({
+        //     path:"applications"
+        // });
+        const job = await Job.findById(jobId);
         if (!job) {
             return res.status(404).json({
                 message: "Jobs not found.",
