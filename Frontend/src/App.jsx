01a34fedbed557ba1ea178e84297e2c64c6f756f
dev15031997 <devashishpal15@gmail.com>
2025-03-27 00:00:26 +0530

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
