7fa853fd91fa6a8b2840564996a74493cfcf5704
dev15031997 <devashishpal15@gmail.com>
2025-04-01 00:33:59 +0530

    Login and Signup page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Home from './components/Home';
 import Navbar from './components/shared/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
-const App = () => {
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  },
+ 
+
+])
+function App() {
+
   return (
-    <>
-      <Navbar/>
-    </>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   )
 }
 
