cabe11a185eb8d1debc4e376ac51c06b70dd2c10
dev15031997 <devashishpal15@gmail.com>
2025-04-01 01:04:08 +0530

    store setup

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
@@ -8,6 +8,9 @@ import { Link,useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import axios from 'axios'
 import { USER_API_END_POINT } from '@/utils/constant'
+import { useDispatch ,useSelector} from 'react-redux'
+import { setLoading } from '@/redux/authSlice'
+import { Loader2 } from 'lucide-react'
 
 const Login = () => {
     const [input, setInput] = useState({
@@ -17,7 +20,8 @@ const Login = () => {
     });
     
     const navigate = useNavigate();
-
+    const dispatch=useDispatch();
+    const { loading } = useSelector(store => store.auth);
 
     const changeEventHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value });
@@ -27,6 +31,7 @@ const Login = () => {
         e.preventDefault();
     
         try {
+            dispatch(setLoading(true));
             const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
                 headers: {
                     "Content-Type": "application/json"
@@ -41,7 +46,7 @@ const Login = () => {
             console.log(error);
             toast.error(error.response.data.message);
         } finally {
-            // 
+            dispatch(setLoading(false));
         }
     }
 
@@ -98,7 +103,9 @@ const Login = () => {
                             </div>
                         </RadioGroup>
                     </div>
-                    <Button type="submit" className="w-full my-4">Login</Button>
+                    {
+                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Login</Button>
+                    }
                     <span className='text-sm'>Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
                 </form>
             </div>
--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Navbar from '../shared/Navbar'
 import {Label} from '../ui/label'
 import {Button} from '../ui/button'
@@ -8,7 +8,9 @@ import { Link,useNavigate} from 'react-router-dom'
 import { USER_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { toast} from 'sonner'
-
+import { useDispatch ,useSelector} from 'react-redux'
+import { setLoading } from '@/redux/authSlice'
+import { Loader2 } from 'lucide-react'
 
 
 const Signup = () => {
@@ -22,10 +24,13 @@ const Signup = () => {
   });
 
   const navigate = useNavigate();
+  const dispatch=useDispatch();
+  const { loading } = useSelector(store => store.auth);
 
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   }
+
   const changeFileHandler = (e) => {
     setInput({ ...input, file: e.target.files?.[0] });
   }
@@ -38,11 +43,14 @@ const Signup = () => {
     formData.append("phoneNumber", input.phoneNumber);
     formData.append("password", input.password);
     formData.append("role", input.role);
+
     if (input.file) {
         formData.append("file", input.file);
     }
 
     try {
+        dispatch(setLoading(true));
+        
         const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
             headers: { 'Content-Type': "multipart/form-data" },
             withCredentials: true,
@@ -55,7 +63,7 @@ const Signup = () => {
         console.log(error);
         toast.error(error.response.data.message);
     } finally{
-        // 
+        dispatch(setLoading(false)); 
     }
 
   }
@@ -141,7 +149,9 @@ const Signup = () => {
                             />
                         </div>
                     </div>
-                    <Button type="submit" className="w-full my-4">Signup</Button>
+                    {
+                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> :   <Button type="submit" className="w-full my-4">Signup</Button>
+                    }
                     <span className='text-sm'>Already have an account? <Link to="/login" className='text-blue-600'>Login</Link></span>
                 </form>
             </div>
