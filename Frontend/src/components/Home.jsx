40d09375c5961543d3f039e44bbd03925e7baf77
dev15031997 <devashishpal15@gmail.com>
2025-04-01 22:52:32 +0530

    Catrgry carousel added

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
 
 const Home = () => {
   return (
