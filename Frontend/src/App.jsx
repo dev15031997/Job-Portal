227dbabe34b06dd44a6ed5eee0b58e53ef371831
dev15031997 <devashishpal15@gmail.com>
2025-04-02 21:28:34 +0530

    Browse Section Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
+import Browse from './components/Browse';
 import Home from './components/Home';
 import Jobs from './components/Jobs';
 import Navbar from './components/shared/Navbar';
@@ -23,6 +24,14 @@ const appRouter = createBrowserRouter([
   {
     path: "/jobs",
     element: <Jobs />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
   },
 ])
 function App() {
