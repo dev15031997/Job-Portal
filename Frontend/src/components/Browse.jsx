9d7ebe229a491a4a6c4f91f16b8bd47b7a08c74f
dev15031997 <devashishpal15@gmail.com>
2025-05-13 00:37:37 +0530

    Browse Jobs

--- Frontend/src/components/Browse.jsx
+++ Frontend/src/components/Browse.jsx
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import Job from './Job';
 import { useDispatch, useSelector } from 'react-redux';
-// import { setSearchedQuery } from '@/redux/jobSlice';
-// import useGetAllJobs from '@/hooks/useGetAllJobs';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import useGetAllJobs from '@/hooks/useGetAllJobs';
 
 const Browse = () => {
-    // useGetAllJobs();
+    useGetAllJobs();
     const {allJobs} = useSelector(store=>store.job);
     const dispatch = useDispatch();
-    // useEffect(()=>{
-    //     return ()=>{
-    //         dispatch(setSearchedQuery(""));
-    //     }
-    // },[])
+    useEffect(()=>{
+        return ()=>{
+            dispatch(setSearchedQuery(""));
+        }
+    },[])
     return (
         <div>
             <Navbar />
